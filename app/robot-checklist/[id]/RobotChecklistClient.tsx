31e2c44ecca9821476dcd18baae144c352665c32
huynhkimthien92-coder <huynhkimthien92@gmail.com>
2026-05-19 09:29:39 +0700

    Update RobotChecklistClient.tsx

--- app/robot-checklist/[id]/RobotChecklistClient.tsx
+++ app/robot-checklist/[id]/RobotChecklistClient.tsx
@@ -1,7 +1,7 @@
 'use client'
 // app/robot-checklist/[id]/RobotChecklistClient.tsx
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { RobotChecklistForm } from '@/components/forms/RobotChecklistForm'
 import { RobotChecklist } from '@/lib/robot-checklist-data'
 
@@ -11,6 +11,12 @@ interface Props {
 
 export default function RobotChecklistClient({ checklist: initial }: Props) {
   const [checklist, setChecklist] = useState<RobotChecklist>(initial)
+
+  // Khi server re-fetch và truyền prop mới xuống (sau router.refresh()),
+  // cập nhật state để component luôn dùng data mới nhất từ DB
+  useEffect(() => {
+    setChecklist(initial)
+  }, [initial])
 
   return (
     <RobotChecklistForm
