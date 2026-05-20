a5afc20e2f2d71e89b73cd389eb73993a483a6da
huynhkimthien92-coder <huynhkimthien92@gmail.com>
2026-05-20 21:17:41 +0700

    Change Navbar layout to always show user info

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -165,7 +165,7 @@ export function Navbar() {
           {/* ===== USER ===== */}
           <div className="flex items-center gap-3">
 
-            <div className="hidden sm:flex flex-col items-end">
+            <div className="flex flex-col items-end">
               <span className="text-sm font-medium">
                 {session?.user?.name}
               </span>
