c8d7695211fd7b975da5b66461087dd7256303a5
huynhkimthien92-coder <huynhkimthien92@gmail.com>
2026-05-21 17:17:04 +0700

    Update MapView.tsx

--- components/5s/MapView.tsx
+++ components/5s/MapView.tsx
@@ -77,7 +77,14 @@ export function MapView({
 
         {/* ===== HEATMAP LAYER ===== */}
         {showHeatmap && (
-          <MapHeatmap issues={issues} />
+          <MapHeatmap
+            issues={issues.map(i => ({
+              ...i,
+              x_percent: i.x_percent ?? 0,
+              y_percent: i.y_percent ?? 0
+            }))}
+          />
+          
         )}
 
         {/* ===== MARKERS ===== */}
