620c1d4e9329a9b148eece7ebf83d478e112aed5
huynhkimthien92-coder <huynhkimthien92@gmail.com>
2026-05-21 16:20:23 +0700

    Update next-auth.d.ts

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,4 +1,5 @@
-import NextAuth from "next-auth"
+import NextAuth, { DefaultSession } from "next-auth"
+
 
 declare module "next-auth" {
   interface Session {
@@ -7,6 +8,6 @@ declare module "next-auth" {
       name?: string | null
       email?: string | null
       image?: string | null
-    }
+    }& DefaultSession["user"]
   }
 }
