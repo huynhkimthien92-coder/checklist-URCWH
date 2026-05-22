81268fa794f230fb971912cb81b83b60a827d5a1
huynhkimthien92-coder <huynhkimthien92@gmail.com>
2026-05-22 13:10:44 +0700

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,1 +1,21 @@
+import pkg from '@prisma/client'
 
+const { PrismaClient } = pkg
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: ReturnType<typeof createPrismaClient> | undefined
+}
+
+function createPrismaClient() {
+  return new PrismaClient({
+    datasourceUrl: process.env.DATABASE_URL
+  })
+}
+
+export const prisma =
+  globalForPrisma.prisma ??
+  createPrismaClient()
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+}
