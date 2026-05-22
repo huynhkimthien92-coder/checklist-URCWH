ee428c35a4e7b2a7d85c8c15fa3096c3e960645b
huynhkimthien92-coder <huynhkimthien92@gmail.com>
2026-05-22 10:03:09 +0700

    Update next-auth.d.ts

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -5,6 +5,7 @@ declare module "next-auth" {
   interface Session {
     user: {
       id: string
+      role: string
       name?: string | null
       email?: string | null
       image?: string | null
