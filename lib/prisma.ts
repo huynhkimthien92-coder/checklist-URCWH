88e540176d1d3203f9c9bd63709634d141c6de2c
huynhkimthien92-coder <huynhkimthien92@gmail.com>
2026-05-22 12:37:44 +0700

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,1 @@
-import pkg from '@prisma/client'
 
-const { PrismaClient } = pkg
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: typeof PrismaClient | undefined
-}
-
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient()
-
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma
-}
