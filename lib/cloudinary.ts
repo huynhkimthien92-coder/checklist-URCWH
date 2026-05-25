22d69fc7c2b114ba5df1d8e67f788bec1d7f071e
huynhkimthien92-coder <huynhkimthien92@gmail.com>
2026-05-25 17:35:15 +0700

    Update cloudinary.ts

--- lib/cloudinary.ts
+++ lib/cloudinary.ts
@@ -58,6 +58,7 @@ export async function uploadToCloudinary(
   form.append('signature', signature)
   form.append('folder', folder)
   if (publicId) form.append('public_id', publicId)
+  form.append('type', 'authenticated') 
   form.append('format', 'webp')
   form.append('quality', 'auto:best')
   form.append('transformation', 'w_1280,c_limit')
